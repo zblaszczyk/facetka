08cfe1f138fc0d2fe932901a1814385d78e4774e
Zbigniew Błaszczyk <zbigniew.blaszczyk@gmail.com>
2025-07-17 16:34:14 +0200

    Add menu

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-document.getElementById('email-link').addEventListener('click', function(event) {
-    event.preventDefault();
-    const email = 'facetkaodmatmy@example.com';
-    navigator.clipboard.writeText(email).then(function() {
-        alert('Email address copied to clipboard!');
-    }, function(err) {
-        console.error('Could not copy text: ', err);
+document.addEventListener('DOMContentLoaded', () => {
+    const emailLink = document.getElementById('email-link');
+    if(emailLink) {
+        emailLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            const email = 'facetkaodmatmy@example.com';
+            navigator.clipboard.writeText(email).then(function() {
+                alert('Email address copied to clipboard!');
+            }, function(err) {
+                console.error('Could not copy text: ', err);
+            });
+        });
+    }
+
+    document.querySelectorAll('.dropdown-content a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 });
